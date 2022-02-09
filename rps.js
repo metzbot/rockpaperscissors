fd003b010007fc7da239efe884248d019ea413dd
Taylor Miller <trmiller89@gmail.com>
2022-02-09 13:34:47 -0800

    overhaul, added basic GUI

--- rps.js
+++ rps.js
@@ -5,6 +5,12 @@ Paper beats rock
 
 Cue up game by writing gamePrompt() in console*/
 
+//global score variables
+let playerScore = 0;
+let computerScore = 0;
+let winner = '';
+let result = '';
+
 //returns random integer from 0 to max
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -49,11 +55,9 @@ function playerScissors(computerSelection) {
     }
 }
 
-//game result variable
-let result = "";
 
 //prompts user to play game
-function gamePrompt() {
+/* function gamePrompt() {
     let choice = prompt("Rock, paper, or scissors?").toLowerCase();
     if ((choice === "rock") || (choice === "paper") || (choice === "scissors")) {
         return result = playRound(choice, computerPlay());
@@ -61,47 +65,100 @@ function gamePrompt() {
         alert("You entered something other than rock, paper, or scissors. Click OK to try again.");
         gamePrompt();
     }
-}
+} */
 
 //plays a single round of rock, paper, scissors
 function playRound(playerSelection, computerSelection) {
     if (checkTie(playerSelection, computerSelection)) {
         return "It's a tie.";
-    } else if (playerSelection === "rock") {
+    } else if (playerSelection === "rockBtn") {
         if (playerRock(computerSelection)) {
+            playerScore++;
             return "You win! Rock smashes scissors.";
         } else {
+            computerScore++;
             return "You lose. Paper wraps rock.";
         }
-    } else if (playerSelection === "paper") {
+    } else if (playerSelection === "paperBtn") {
         if (playerPaper(computerSelection)) {
+            playerScore++;
             return "You win! Paper wraps rock.";
         } else {
+            computerScore++;
             return "You lose. Scissors cuts paper.";
         }
-    } else if (playerSelection === "scissors") {
+    } else if (playerSelection === "scissorsBtn") {
         if (playerScissors(computerSelection)) {
+            playerScore++;
             return "You win! Scissors cuts paper.";
         } else {
+            computerScore++;
             return "You lose. Rock smashes scissors.";
         }
     }
 }
 
-function game() {
+/* let roundResult;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        roundResult = playRound(button.id, computerPlay());
+        const scoreContainer = document.querySelector('score');
+        const content = document.createElement('div');
+        content.classList.add('content');
+        content.textContent = roundResult;
+        scoreContainer.appendChild(content);
+    });
+}); */
+
+const scoreInfo = document.getElementById('scoreInfo');
+const scoreMessage = document.getElementById('scoreMessage');
+const playerScoreDisp = document.getElementById('playerScore');
+const computerScoreDisp = document.getElementById('computerScore');
+
+/* rockBtn.addEventListener('click', () => {
+    result = playRound('rock', computerPlay()); });
+paperBtn.addEventListener('click', () => {
+    result = playRound('paper', computerPlay()); });
+scissorsBtn.addEventListener('click', () => {
+    result = playRound('scissors', computerPlay()); }); */
+
+const btns = document.querySelectorAll('button');
+const content = document.createElement('div');
+const playerScoreCount = document.getElementById('player');
+const compScoreCount = document.getElementById('computer');
+btns.forEach((button) => {
+    button.addEventListener('click', () => {
+        result = playRound(button.id, computerPlay());
+        content.classList.add('content');
+        content.textContent = result;
+        scoreMessage.appendChild(content);
+        playerScoreCount.textContent = `Player: ${playerScore}`
+        compScoreCount.textContent = `Computer: ${computerScore}`;
+        if ( (playerScore === 5) || (computerScore === 5) ) {
+            if ( playerScore > computerScore ) {
+                content.textContent = 'Congratulations, you won five rounds!';
+            } else { content.textContent = 'You\'re clearly terrible at this game! You lost!' };
+        }
+    })});
+
+
+
+/* function game() {
     for (let step = 0; step < 5; step++) {
         gamePrompt();
         console.log(result);
     }
-}
+} */
 
-function printEvens() {
-    for (let i = 0; i <= 10; i++) {
-        if (i % 2 == 0) {
-            console.log("even")
-        } else { console.log(`${i}`)}
-    }
-}
+//bunch of random junk down here
+// function printEvens() {
+//     for (let i = 0; i <= 10; i++) {
+//         if (i % 2 == 0) {
+//             console.log("even")
+//         } else { console.log(`${i}`)}
+//     }
+// }
 
 
 /* for each i in the interval {
@@ -111,12 +168,12 @@ function printEvens() {
 } */
 
 
-function printPrimes(n) {
-    nextPrime:
-    for (let i = 2; i <= n; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) continue nextPrime;
-        }
-        console.log(i);
-    }
-}
+// function printPrimes(n) {
+//     nextPrime:
+//     for (let i = 2; i <= n; i++) {
+//         for (let j = 2; j < i; j++) {
+//             if (i % j == 0) continue nextPrime;
+//         }
+//         console.log(i);
+//     }
+// }
